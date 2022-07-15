611de03914e803c1f80818b9d7f75b0deb5dc0f8
boioib <109253331+boioib@users.noreply.github.com>
2022-07-15 16:29:58 -0700

    Update _c_logical_turtle.py

--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -13,15 +13,19 @@ def screen_clicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
+    a_turtle.penup()
     # 7. Move the turtle to a new location using .goto(x, y)
-
+    a_turtle.goto(x=10, y=20)
 
 def turtle_clicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+
+    for i in range(3):
+        a_turtle.right(360)
+        a_turtle.color(get_random_color())
+
         # 9. Make the turtle spin 360 degrees using the .right() method
         
         # 10. Use the .color() method and getRandomColor() function to change
@@ -34,16 +38,18 @@ if __name__ == '__main__':
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    a_turtle = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    a_turtle.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    a_turtle.color('orange')
+    a_turtle.pencolor('blue')
+
     # 4. Set and new width, length, and outline of our turtle
-    #    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+    a_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
 
     # 5. Uncomment the following line and replace 'my_turtle' with your turtle
-    # my_turtle.onclick(turtle_clicked)
+    a_turtle.onclick(turtle_clicked)
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
