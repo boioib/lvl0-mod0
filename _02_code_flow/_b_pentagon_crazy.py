b90549f19a2fe5df74f169233cb9ef6d67da9764
boioib <109253331+boioib@users.noreply.github.com>
2022-07-15 16:32:16 -0700

    Update _b_pentagon_crazy.py

--- _02_code_flow/_b_pentagon_crazy.py
+++ _02_code_flow/_b_pentagon_crazy.py
@@ -21,19 +21,29 @@ if __name__ == '__main__':
     colors = ('red', 'blue', 'green', 'yellow', 'orange')
     
     # Make a new turtle
-    
+    turtle_stuart = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    turtle_stuart.shape('turtle')
     # Set the turtle speed to max (0)
-    
+    turtle_stuart.speed(0)
     # Set the turtle width to 1
-    
+    turtle_stuart.width(1)
     # Create a variable to hold the number of sides in a pentagon
-    
+    number = 5
     # Create a variable to be the angle of 360 divided by the sides variable
-    
+    penta_angle = 360/5
     # Use a for loop to repeat ALL the following lines of code 360 times. 
+    for i in range(360):
+        if i == 100:
+            turtle_stuart.width(2)
+        if i == 200:
+            turtle_stuart.width(3)
+        turtle_stuart.pencolor(get_next_color(i))
+        turtle_stuart.forward(i)
+        turtle_stuart.right(penta_angle + 1)
+        turtle_stuart.hideturtle()
         
+
         # If the loop variable (i) is equal to 100, set the turtle width to 2
         
         # If the loop variable (i) is equal to 200, set the turtle width to 3
